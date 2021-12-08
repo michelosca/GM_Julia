08bd81ce5ba0a0366f781ddc33cfe72c009b5ce9
michel.osca <michel.osca@york.ac.uk>
2021-12-08 09:06:04 +0100

    plotmodule changes

--- PlotModule.jl
+++ PlotModule.jl
@@ -8,9 +8,12 @@ using SharedData: K_to_eV
 function PlotDensities_Charged(output::OutputBlock,
     species_list::Vector{Species})
 
-    p = plot(xlabel = "t [ms]", ylabel = "n [m^-3]",
-        xscale = :log10, xlims = (5.e-2, 1.e4), yscale = :log10,
-        ylims = (1.e14, 1.e17))
+    p = plot()#xlabel = "t [ms]",
+        #ylabel = "n [m^-3]",
+        #xscale = :log10,
+        #xlims = (5.e-2, 1.e4),
+        #yscale = :log10,
+        #ylims = (1.e14, 1.e17))
 
     time = output.x#/1.e-3
     for s in species_list
@@ -32,7 +35,7 @@ function PlotDensities_Neutral(output::OutputBlock,
     p = plot(xlabel = "t [ms]", ylabel = "n [m^-3]")#,
     #yscale = :log10, xlims = (1.e15, 1.e22))
 
-    time = output.x/1.e-3
+    time = output.x / 5.0
     for s in species_list
         if !s.has_dens_eq
             continue
@@ -49,9 +52,9 @@ end
 function PlotTemperatures_Charged(output::OutputBlock,
     species_list::Vector{Species})
 
-    p = plot(xlabel = "t [ms]", ylabel = "T [eV]",
-        xscale = :log10, xlims = (5.e-2, 1.e4),
-        ylims = (0, 5))
+    p = plot(xlabel = "t [ms]", ylabel = "T [eV]")#,
+        #xscale = :log10, xlims = (5.e-2, 1.e4),
+        #ylims = (0, 5))
 
     time = output.x#/1.e-3
     for s in species_list
@@ -89,8 +92,11 @@ end
 function PlotRateCoefficients(output::OutputBlock,
     reaction_list::Vector{Reaction}, reaction_id::Int64 = 0)
 
-    p = plot(xlabel = "t [ms]", ylabel = "m^3/s",
-        yscale = :log10, ylims = (1.e-20, 1.e-10), legend=:outertopright)#,
+    p = plot(xlabel = "Reaction ID", ylabel = "",
+        kind = "bar",
+        yscale = :log10)
+        #ylims = (1.e-20, 1.e-10),
+        #legend=:outertopright,
         #ylims = (0, 5))
     time = output.x#/1.e-3
     if reaction_id == 0
@@ -98,8 +104,16 @@ function PlotRateCoefficients(output::OutputBlock,
             K = output.K[r.id]
             if K == Float64[]
                 continue
+            elseif K[end] == 0
+                continue
             else
-                plot!(p, time, K, label=r.name, w = 2, yscale=:log10)
+                dens_list = output.n[r.reactant_species]
+                dens = 1
+                for n in dens_list
+                    dens *= n[end]
+                end
+                #print("ID ", r.id, " dens product ", dens,"  K ", K[end]," Kn ",K[end]*dens ,"\n")
+                plot!(p, [r.id], [K[end]*dens], w = 2, markershape=:circle, legend = false)
             end
         end
     else
